4234547990c800519bebc6fc92e56cd09612ce28
krugersu <pk69@kruger.su>
2017-10-11 09:49:49 +0700

    Научился ходить

--- game.py
+++ game.py
@@ -14,17 +14,21 @@ import data
 def m_NewGame():
     data.playfield = [6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0] #Игровое поле
     print(data.playfield)
-    m_Move(0,0)
     
-#v_box -  Номер коробочки с которой начинается ход    
+    
+#v_box -  Номер коробочки с которой начинается ход
+#h_g   -  Чей ход 0 - машины  1 - человека 
 def m_Move(v_box,h_g):
     
     v_stone = data.playfield[v_box] # Количество комней в коробочке 
+    print(v_stone)
     data.playfield[v_box] = 0 # Забрали все камни из коробочки
     
     # Раскладывание камней
     while v_stone != 0:
         v_box += 1
+        if v_box == 14:
+            v_box = 0
         data.playfield[v_box] += 1
         v_stone -= 1
         
@@ -32,13 +36,17 @@ def m_Move(v_box,h_g):
     
     if not data.v_repeat_stroke and v_box < 7: # Закончили в своей коробочке
         # Проверка на то, что закончили в пустой коробочке
-        if  data.playfield[v_box] == 1 and h_g == 0:
-            data.playfield[data.n_m_Kalach] = data.playfield[data.n_m_Kalach] + 1 
-        
+        if  data.playfield[v_box] == 1 and h_g == 0 and data.playfield[data.map_box[v_box]] != 0:
+            data.playfield[data.n_m_Kalach] = data.playfield[data.n_m_Kalach] + 1 + data.playfield[data.map_box[v_box]]
+            data.playfield[v_box] = 0
+    data.w_step = 1        
     
 
 def m_HumanMove(n_Box):
     pass
 
 def m_FirstMove():
+    pass
+
+def m_analis(): 
     pass
--- main.py
+++ main.py
@@ -12,15 +12,23 @@ import data
 
 
 
-
-
 def main(arguments):
-    
+    data.w_step = 0
     game.m_NewGame()
+    
+    while  data.w_step != 2:
+        print(data.playfield)
+        if data.w_step == 1:
+            box = int(input('Какой номер коробочки? '))
+            game.m_Move(box,data.w_step)
+        else:
+            game.m_analis()
+            game.m_Move(0,data.w_step)
+            
    # game.m_Move()
-    print(data.playfield)
-    if len(data.playfield) > 0: 
-        data.playfield[3] = 8
+    
+    #if len(data.playfield) > 0: 
+      #  data.playfield[3] = 8
       #  print(data.playfield)
     
 
